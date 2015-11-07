8ab9e7b2364579dfe3ad93dfbf4403f5ebd956b4
snowstop19900327 <hst790327@gmail.com>
2015-11-07 09:21:09 +0800

    初步修改樣式
    還沒完全好 基本樣式已調整 名稱已換

---
+++ js/mymap.js
@@ -0,0 +1,206 @@
+[
+    {
+        "featureType": "all",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": "30"
+            },
+            {
+                "lightness": "-1"
+            },
+            {
+                "gamma": "1.29"
+            }
+        ]
+    },
+    {
+        "featureType": "all",
+        "elementType": "labels.text",
+        "stylers": [
+            {
+                "weight": "0.81"
+            },
+            {
+                "lightness": "20"
+            },
+            {
+                "saturation": "-32"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "all",
+        "stylers": [
+            {
+                "lightness": "-22"
+            },
+            {
+                "saturation": "-53"
+            },
+            {
+                "gamma": "1.38"
+            },
+            {
+                "color": "#336884"
+            },
+            {
+                "weight": "0.71"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": "1"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape.man_made",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "weight": 0.9
+            },
+            {
+                "visibility": "off"
+            },
+            {
+                "lightness": "8"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#83cead"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#c4d5db"
+            },
+            {
+                "lightness": "10"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels.text",
+        "stylers": [
+            {
+                "saturation": "-15"
+            },
+            {
+                "lightness": "-34"
+            },
+            {
+                "weight": "0.90"
+            },
+            {
+                "gamma": "0.90"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#89d0f0"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "weight": "1.68"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#b5daea"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "weight": "4.08"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "labels.text",
+        "stylers": [
+            {
+                "lightness": "-31"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#3ca7dd"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "labels.text",
+        "stylers": [
+            {
+                "saturation": "78"
+            },
+            {
+                "lightness": "-51"
+            },
+            {
+                "gamma": "3.40"
+            }
+        ]
+    }
+]
